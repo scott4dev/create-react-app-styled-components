53675aca9b7f75402a459e3a9f84705f5c143412
Claudio Maccari <scott4dev@users.noreply.github.com>
2018-03-13 09:32:18 +0100

    Optimize import

--- src/styles.js
+++ src/styles.js
@@ -1,5 +1,4 @@
-import styled from 'styled-components';
-import {keyframes} from 'styled-components'
+import styled, { keyframes } from 'styled-components';
 
 const spinLogoAnimation = keyframes`
   from { transform: rotate(0deg); }
